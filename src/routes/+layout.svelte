41c345505a93176939402bf265e9ae3ff541734b
Luca Martinelli <martinelliluca98@gmail.com>
2022-10-07 20:50:32 +0200

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  export const prerender = true;
+  
+  import "../app.css";
+  import Header from '$lib/components/Header.svelte'
+  import Footer from '$lib/components/Footer.svelte'
+</script>
+
+<Header />
+
+<main class="my-16 min-h-screen px-3">
+  <div class="max-w-screen-lg mx-auto">
+    <slot />
+  </div>
+</main>
+
+<Footer />
