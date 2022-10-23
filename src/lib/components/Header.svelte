2c8c176d3c6e3e210134e1f39ead10da5ead8631
Luca Martinelli <martinelliluca98@gmail.com>
2022-10-23 13:47:56 +0200

    commit

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,7 +4,7 @@
 
 <svelte:window bind:scrollY={y} />
 
-<div class="z-50 px-3 bg-white fixed left-0 right-0 top-0 transition-shadow duration-300 {y > 0 ? 'shadow-md' : ''}">
+<div class="z-50 px-3 bg-white fixed left-0 right-0 top-0 transition-shadow duration-300 {y > 0 ? 'md:shadow-md' : ''}">
   <header class="flex py-5 items-center gap-5 max-w-screen-xl mx-auto">
     <a href="/"><img class="h-10" src="/logo.svg" alt="Digital People" /></a>
     <span class="flex-1" />
