8a2f038fe929eb5b007c11024a1147424eccc315
Luca Martinelli <martinelliluca98@gmail.com>
2024-03-08 09:58:59 +0100

    Fix

--- src/routes/podcasts/[slug]/rss/+server.js
+++ src/routes/podcasts/[slug]/rss/+server.js
@@ -54,9 +54,9 @@ const xml =
       </itunes:owner>
       <copyright>${podcast.copyright || "Creative Commons Attribuzione - Non opere derivate 4.0 Internazionale"}</copyright>
       
-      <itunes:image href="${podcast.image.startsWith("http") ? podcast.image : uri + podcast.imageset[2]}?t=3" />
+      <itunes:image href="${podcast.image.startsWith("http") ? podcast.image : uri + podcast.imageset[2]}?t=4" />
       <image>
-        <url>${podcast.image.startsWith("http") ? podcast.image : uri + podcast.imageset[2]}?t=3</url>
+        <url>${podcast.image.startsWith("http") ? podcast.image : uri + podcast.imageset[2]}?t=4</url>
         <title>
           <![CDATA[
             ${podcast.title}
